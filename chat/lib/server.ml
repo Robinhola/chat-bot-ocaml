28ea9a7f83686bdb02f25990e01eab51f37bf78e
Robin Elrharbi-Fleury <robin.elrharbifleury@gmail.com>
2024-11-16 19:26:47 +0000

    Refactoring

--- chat/lib/server.ml
+++ chat/lib/server.ml
@@ -2,6 +2,7 @@ open! Base
 open! Core
 open! Async
 
+(* Use an Mvar to make sure we don't erase messages while reading *)
 let with_can_write can_write ~f =
   let%bind () = Mvar.take can_write in
   let result = f () in
@@ -18,7 +19,6 @@ let enqueue_msg ~can_write ~msg_queues reader =
   | `Ok msg ->
     with_can_write can_write ~f:(fun () ->
       Log.Global.debug_s [%message "Read" (msg : string)];
-      Log.Global.debug_s [%message "Update all msgs queues..."];
       Hashtbl.iteri msg_queues ~f:(fun ~key ~data:queue ->
         Log.Global.debug_s [%message "Adding msg into queue" (key : int)];
         Queue.enqueue queue msg))
@@ -36,63 +36,71 @@ let user_left ~can_write ~msg_queues =
   write_server_message ~can_write ~msg_queues msg
 ;;
 
+type 'a t =
+  { writer : Writer.t
+  ; can_write : (unit, 'a) Mvar.t
+  ; id_to_msg_queue : (int, string Queue.t) Hashtbl.t
+  ; ws : Websocket.t
+  }
+
+let rec loop ~id t =
+  let { writer; can_write; id_to_msg_queue; ws } = t in
+  Log.Global.debug_s [%message "Echo loop" (id : int)];
+  match Writer.is_closed writer with
+  | false ->
+    let%bind msgs =
+      with_can_write can_write ~f:(fun () ->
+        let queue = Hashtbl.find_exn id_to_msg_queue id in
+        let msgs = Queue.to_list queue in
+        let () = Queue.clear queue in
+        msgs)
+    in
+    let%bind () =
+      Deferred.List.iter ~how:`Sequential msgs ~f:(fun msg ->
+        let _, wswriter = Websocket.pipes ws in
+        Log.Global.debug_s [%message "Send msg" (id : int) (msg : string)];
+        Pipe.write_if_open wswriter msg)
+    in
+    let%bind () = Clock.after (Time_float_unix.Span.of_ms 100.) in
+    loop ~id t
+  | true ->
+    Log.Global.info_s [%message "Bye" (id : int)];
+    Hashtbl.remove id_to_msg_queue id;
+    user_left ~can_write ~msg_queues:id_to_msg_queue
+;;
+
 let start ~port =
   let global_i = ref 0 in
-  let index_to_msg_queue = Hashtbl.create (module Int) in
+  let id_to_msg_queue = Hashtbl.create (module Int) in
   Tcp.Server.create
     ~on_handler_error:`Raise
     (Tcp.Where_to_listen.of_port port)
     (fun _address reader writer ->
-       let i = !global_i in
+       let id = !global_i in
        global_i := !global_i + 1;
-       Log.Global.info_s [%message "Hello!" (i : int)];
+       Log.Global.info_s [%message "Hello!" (id : int)];
        let can_write = Mvar.create () in
-       let () = Hashtbl.add_exn index_to_msg_queue ~key:i ~data:(Queue.create ()) in
+       let () = Hashtbl.add_exn id_to_msg_queue ~key:id ~data:(Queue.create ()) in
        let ws = Websocket.create ~role:Server reader writer in
-       let _wsreader, _wswriter = Websocket.pipes ws in
        let () =
          Websocket.monitor_pongs
            ~ping_every:(Time_ns.Span.of_int_sec 3)
            ~concerning_pong_response_delay:(Time_ns.Span.of_int_sec 10)
            ~on_concerning_pong_response_delay:(fun () ->
-             Log.Global.info_s [%message "Lost connection!" (i : int)])
+             Log.Global.info_s [%message "Lost connection!" (id : int)];
+             don't_wait_for (Writer.close writer))
            ws
        in
-       let wsreader, wswriter = Websocket.pipes ws in
        let receiver = Websocket.frame_received ws in
        (* We could unsubscribe from annoying clients *)
        let _bus =
          Bus.subscribe_exn receiver [%here] ~f:(function
            | Text ->
-             enqueue_msg ~can_write ~msg_queues:index_to_msg_queue wsreader
-             |> don't_wait_for
+             let wsreader, _ = Websocket.pipes ws in
+             enqueue_msg ~can_write ~msg_queues:id_to_msg_queue wsreader |> don't_wait_for
            | opcode -> Opcode.log opcode)
        in
        let%bind () = Mvar.put can_write () in
-       let rec loop () =
-         Log.Global.debug_s [%message "Echo loop" (i : int)];
-         match Writer.is_closed writer with
-         | false ->
-           (* Use an Mvar here to make sure we don't erase messages while reading *)
-           let%bind msgs =
-             with_can_write can_write ~f:(fun () ->
-               let queue = Hashtbl.find_exn index_to_msg_queue i in
-               let msgs = Queue.to_list queue in
-               let () = Queue.clear queue in
-               msgs)
-           in
-           let%bind () =
-             Deferred.List.iter ~how:`Sequential msgs ~f:(fun msg ->
-               Log.Global.info_s [%message "Send msg" (i : int) (msg : string)];
-               Pipe.write_if_open wswriter msg)
-           in
-           let%bind () = Clock.after (Time_float_unix.Span.of_ms 100.) in
-           loop ()
-         | true ->
-           Log.Global.info_s [%message "Bye" (i : int)];
-           Hashtbl.remove index_to_msg_queue i;
-           user_left ~can_write ~msg_queues:index_to_msg_queue
-       in
-       loop ())
+       loop ~id { writer; can_write; id_to_msg_queue; ws })
   >>= Tcp.Server.close_finished
 ;;

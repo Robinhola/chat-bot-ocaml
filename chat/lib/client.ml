dd5af701ae766e36c53bd2e0402272a925a5052a
Robin Elrharbi-Fleury <robin.elrharbifleury@gmail.com>
2024-11-16 11:19:07 +0000

    Receiving pings and handling disconnect

--- chat/lib/client.ml
+++ chat/lib/client.ml
@@ -9,9 +9,14 @@ let start ~host ~port =
     (Tcp.Where_to_connect.of_host_and_port host_and_port)
     (fun _socket reader writer ->
        let ws = Websocket.create ~role:Client reader writer in
+       let receiver = Websocket.frame_received ws in
+       let _bus =
+         Bus.subscribe_exn receiver [%here] ~f:(fun opcode ->
+           Log.Global.debug_s [%message "Sub" (opcode : Websocket.Opcode.t)])
+       in
        let rec echo_loop () =
          Log.Global.debug_s [%message "Send ping"];
-         Websocket.send_ping ws "Hello";
+         (* Websocket.send_ping ws "Hello"; *)
          let%bind () = Clock.after (Time_float_unix.Span.of_ms 1000.) in
          echo_loop ()
        in
--- chat/lib/server.ml
+++ chat/lib/server.ml
@@ -9,10 +9,15 @@ let start ~port =
     (fun _address reader writer ->
        let ws = Websocket.create ~role:Server reader writer in
        let rec echo_loop () =
-         Log.Global.debug_s [%message "Send ping"];
-         Websocket.send_ping ws "Hello";
-         let%bind () = Clock.after (Time_float_unix.Span.of_ms 1000.) in
-         echo_loop ()
+         match Writer.is_closed writer with
+         | false ->
+           Log.Global.debug_s [%message "Send ping"];
+           Websocket.send_ping ws "Hello";
+           let%bind () = Clock.after (Time_float_unix.Span.of_ms 1000.) in
+           echo_loop ()
+         | true ->
+           Log.Global.debug_s [%message "Bye"];
+           Deferred.unit
        in
        echo_loop ())
   >>= Tcp.Server.close_finished
